610a9a3f31794396e799e33814967c2b67216094
Stephen James <stephenjames@me.com>
2014-09-10 09:18:44 +0100

    modify build.bat to use `call npm install instead of just `npm install`.   Without call, npm exits the batch file on completion.  Added clean npm modules folder to .bat and .sh - older modules were causing errors, so best to clean anyhow.

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-npm install
+rd /s /q node_modules
+call npm install
 git submodule update --init --recursive
 cd server
 msbuild /p:Platform="Any CPU"
+cd ..
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+rm -rf node_modules
 npm install
 git submodule update --init --recursive
 cd server
 xbuild /p:Platform="Any CPU"
+cd ..
