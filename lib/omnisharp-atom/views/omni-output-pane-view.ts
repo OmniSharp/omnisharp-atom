5a178e45cd45b6f577bbcf30d08657996ab4943d
David Driscoll <david.driscoll@gmail.com>
2015-04-08 07:39:41 -0400

    Convert omni-output-pane-view.coffee -> omni-output-pane-view.ts

--- lib/omnisharp-atom/views/omni-output-pane-view.ts
+++ lib/omnisharp-atom/views/omni-output-pane-view.ts
@@ -1,49 +1,96 @@
-{View}  = require 'atom-space-pen-views'
-{$} = require 'atom-space-pen-views'
-Convert = require 'ansi-to-html'
-Vue = require 'vue'
-_ = require 'lodash'
+var spacePenViews = require('atom-space-pen-views')
+var View = <any>spacePenViews.View;
+var Convert = require('ansi-to-html')
+import Vue = require('vue')
+import _ = require('lodash')
+import OmniSharpServer = require('../../omni-sharp-server/omni-sharp-server')
 
-OmniSharpServer = require '../../omni-sharp-server/omni-sharp-server'
+// Internal: A tool-panel view for the test result output.
+class OmniOutputPaneView extends View {
+    private vm : {uninitialized: boolean; initialized: boolean; output: any[] };
+    private convert: typeof Convert;
 
-module.exports =
-# Internal: A tool-panel view for the test result output.
-class OmniOutputPaneView extends View
-  @content: ->
-    @div class: 'omni-output-pane-view', =>
-      @ul class: 'background-message centered', 'v-class': 'hide: initialized', =>
-        @li =>
-          @span 'Omnisharp server is turned off'
-          @kbd class: 'key-binding text-highlight', '⌃⌥O'
-      @div class: 'messages-container', 'v-class': 'hide: uninitialized', =>
-        @pre 'v-class': 'text-error: l.isError', 'v-repeat': 'l :output', '{{ l.message | ansi-to-html }}'
+    public static content() {
+        return this.div({
+            "class": 'omni-output-pane-view'
+        }, () => {
+                this.ul({
+                    "class": 'background-message centered',
+                    'v-class': 'hide: initialized'
+                }, () => {
+                        return this.li(() => {
+                            this.span('Omnisharp server is turned off');
+                            return this.kbd({
+                                "class": 'key-binding text-highlight'
+                            }, '⌃⌥O');
+                        });
+                    });
+                return this.div({
+                    "class": 'messages-container',
+                    'v-class': 'hide: uninitialized'
+                }, () => {
+                        return this.pre({
+                            'v-class': 'text-error: l.isError',
+                            'v-repeat': 'l :output'
+                        }, '{{ l.message | ansi-to-html }}');
+                    });
+            });
+    }
 
-  initialize: ->
-    scrollToBottom= _.throttle (=>this.find(".messages-container")[0].lastElementChild?.scrollIntoViewIfNeeded()), 100
-    Vue.filter 'ansi-to-html', (value) =>
-      scrollToBottom()
-      @convert ?= new Convert()
-      v = @convert.toHtml value
-      v.trim()
+    public initialize() {
+        var scrollToBottom;
+        scrollToBottom = _.throttle(() => {
+            var ref;
+            return (ref = this.find(".messages-container")[0].lastElementChild) != null ? ref.scrollIntoViewIfNeeded() : void 0;
+        }, 100);
+        Vue.filter('ansi-to-html', (value) => {
+            var v;
+            scrollToBottom();
+            if (this.convert == null) {
+                this.convert = new Convert();
+            }
+            v = this.convert.toHtml(value);
+            return v.trim();
+        });
+        var viewModel = new Vue({
+            el: this[0],
+            data: _.extend(OmniSharpServer.vm, {
+                uninitialized: true,
+                initialized: false,
+                output: []
+            })
+        });
+        this.vm = <any>viewModel;
+        atom.on("omni-sharp-server:out", (data) => {
+            if (this.vm.output.length >= 1000) {
+                this.vm.output.$remove(0);
+            }
+            return this.vm.output.push({
+                message: data
+            });
+        });
+        atom.on("omni-sharp-server:err", (data) => {
+            if (this.vm.output.length >= 1000) {
+                this.vm.output.$remove(0);
+            }
+            return this.vm.output.push({
+                message: data,
+                isError: true
+            });
+        });
+        return atom.on("omni-sharp-server:start", (pid, port) => {
+            this.vm.uninitialized = false;
+            this.vm.initialized = true;
+            this.vm.output = [];
+            return this.vm.output.push({
+                message: "Starting Omnisharp server (pid:" + pid + ", port:" + port + ")"
+            });
+        });
+    }
 
-    @vm = new Vue
-      el: this[0]
-      data:_ .extend OmniSharpServer.vm,
-        uninitialized: true
-        initialized: false
-        output: []
+    destroy() {
+        this.detach()
+    }
+}
 
-    atom.on "omni-sharp-server:out", (data) =>
-      @vm.output.$remove(0) if @vm.output.length >= 1000
-      @vm.output.push message: data
-    atom.on "omni-sharp-server:err", (data) =>
-      @vm.output.$remove(0) if @vm.output.length >= 1000
-      @vm.output.push {message: data, isError: true}
-    atom.on "omni-sharp-server:start", (pid, port) =>
-      @vm.uninitialized = false
-      @vm.initialized = true
-      @vm.output = []
-      @vm.output.push message:"Starting Omnisharp server (pid:#{pid}, port:#{port})"
-
-  destroy: ->
-    @detach()
+export = OmniOutputPaneView;
