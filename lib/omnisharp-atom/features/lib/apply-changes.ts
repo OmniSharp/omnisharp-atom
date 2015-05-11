2950bbca17af9a434fbdd7be8b45675a7a6516df
nosami <jasonimison@gmail.com>
2015-05-11 05:24:35 +0100

    make changes atomic

--- lib/omnisharp-atom/features/lib/apply-changes.ts
+++ lib/omnisharp-atom/features/lib/apply-changes.ts
@@ -3,11 +3,12 @@ var Range = require('atom').Range;
 class Changes {
     public static applyChanges(editor: Atom.TextEditor, changes: OmniSharp.Models.LinePositionSpanTextChange[]) {
         var buffer = editor.getBuffer();
-
+        var checkpoint = buffer.createCheckpoint();
         changes.forEach((change) => {
             var range = new Range([change.StartLine - 1, change.StartColumn - 1], [change.EndLine - 1, change.EndColumn - 1]);
             buffer.setTextInRange(range, change.NewText);
         });
+        buffer.groupChangesSinceCheckpoint(checkpoint);
     }
 }
 
