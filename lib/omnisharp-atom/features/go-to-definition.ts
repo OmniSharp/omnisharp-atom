8be174518a9cf3ac8d593d4c6b17c166a23e44a2
David Driscoll <david.driscoll@gmail.com>
2015-06-30 15:37:22 -0400

    Fixed go-to-defition to work properly (it was broken the same time the tooltip was broken).  Also once we get a metadata end point, we can use local identifiers for go-to-def

--- lib/omnisharp-atom/features/go-to-definition.ts
+++ lib/omnisharp-atom/features/go-to-definition.ts
@@ -3,6 +3,9 @@ import {CompositeDisposable, Observable} from "rx";
 import Omni = require('../../omni-sharp-server/omni');
 import $ = require('jquery');
 var Range = require('atom').Range;
+import {highlight} from "./highlight";
+
+var identifierRegex = /^identifier|identifier$|\.identifier\./;
 
 class GoToDefinition implements OmniSharp.IFeature {
     private disposable: Rx.CompositeDisposable;
@@ -11,34 +14,63 @@ class GoToDefinition implements OmniSharp.IFeature {
 
     public activate() {
         this.disposable = new CompositeDisposable();
-        this.disposable.add(Omni.editors.subscribe((editor: Atom.TextEditor) => _.defer(() => {
+        this.disposable.add(Omni.activeEditor.where(z => !!z).subscribe((editor: Atom.TextEditor) => {
+            var cd = new CompositeDisposable();
+            cd.add(Omni.activeEditor.where(e => e !== editor).subscribe(() => {
+                cd.dispose();
+                this.disposable.add(cd);
+            }));
+
             var view = $(atom.views.getView(editor));
             var scroll = this.getFromShadowDom(view, '.scroll-view');
-            var mousemove = Observable.fromEvent<MouseEvent>(scroll[0], 'mousemove');
             var click = Observable.fromEvent<MouseEvent>(scroll[0], 'click');
+
+            var mousemove = Observable.fromEvent<MouseEvent>(scroll[0], 'mousemove')
+                .shareReplay(1);
+
+            var keyup = Observable.fromEvent<KeyboardEvent>(view[0], 'keyup')
+                .where(x => x.which === 17 || x.which === 224 || x.which === 93)
+                .throttleFirst(100);
+
+            var keydown = Observable.fromEvent<KeyboardEvent>(view[0], 'keydown')
+                .where(z => !z.repeat)
+                .where(e => e.ctrlKey || e.metaKey)
+                .throttleFirst(100);
+
+            var specialKeyDown = keydown
+                .flatMapLatest(x => mousemove
+                    .takeUntil(keyup)
+                    .map(event => {
+                        var pixelPt = this.pixelPositionFromMouseEvent(editor, view, event);
+                        var screenPt = editor.screenPositionForPixelPosition(pixelPt);
+                        return { event, bufferPt: <TextBuffer.Point>editor.bufferPositionForScreenPosition(screenPt) };
+                    })
+                    .distinctUntilChanged(e => e.bufferPt, (current, next) => current.isEqual(<any>next)));
 
             // to debounce mousemove event's firing for some reason on some machines
             var lastExprTypeBufferPt: any;
 
-            var cd = new CompositeDisposable();
+            cd.add(mousemove.subscribe(() => { }));
+            editor.onDidDestroy(() => cd.dispose());
 
-            cd.add(mousemove.subscribe((e) => {
-                if (!e.ctrlKey && !e.metaKey) {
-                    this.removeMarker();
-                    return;
+            var eventDisposable: Rx.Disposable;
+            cd.add(highlight.observe.enabled.subscribe((enabled: boolean) => {
+                var mouseObservable = specialKeyDown;
+
+                // Old behavior waited to mark
+                if (!(highlight.enabled && highlight.active))
+                    mouseObservable = mouseObservable.delay(100);
+
+                if (eventDisposable) {
+                    eventDisposable.dispose();
+                    cd.remove(eventDisposable);
                 }
 
-                var pixelPt = this.pixelPositionFromMouseEvent(view, e)
-                var screenPt = editor.screenPositionForPixelPosition(pixelPt)
-                var bufferPt = editor.bufferPositionForScreenPosition(screenPt)
-                if (lastExprTypeBufferPt && lastExprTypeBufferPt.isEqual(bufferPt))
-                    return;
-
-                lastExprTypeBufferPt = bufferPt;
-
-                this.clearExprTypeTimeout();
-                this.exprTypeTimeout = setTimeout(() => this.underlineIfNavigable(editor, view, e), 100);
+                eventDisposable = mouseObservable.subscribe((e) => this.underlineIfNavigable(editor, e));
+                cd.add(eventDisposable);
             }));
+
+            cd.add(keyup.subscribe(() => this.removeMarker()));
 
             cd.add(click.subscribe((e) => {
                 if (!e.ctrlKey && !e.metaKey) {
@@ -48,7 +80,7 @@ class GoToDefinition implements OmniSharp.IFeature {
                 this.goToDefinition();
             }));
             this.disposable.add(cd);
-        })));
+        }));
 
         this.disposable.add(atom.emitter.on("symbols-view:go-to-declaration", this.goToDefinition));
         this.disposable.add(Omni.addTextEditorCommand("omnisharp-atom:go-to-definition", this.goToDefinition));
@@ -81,11 +113,7 @@ class GoToDefinition implements OmniSharp.IFeature {
         }
     }
 
-    private underlineIfNavigable(editor, view, event) {
-        var pixelPt = this.pixelPositionFromMouseEvent(view, event);
-        var screenPt = editor.screenPositionForPixelPosition(pixelPt);
-        var bufferPt = editor.bufferPositionForScreenPosition(screenPt);
-
+    private underlineIfNavigable(editor: Atom.TextEditor, { event, bufferPt }: { event: MouseEvent, bufferPt: TextBuffer.Point }) {
         var buffer = editor.getBuffer();
         var startColumn = bufferPt.column;
         var endColumn = bufferPt.column;
@@ -106,25 +134,45 @@ class GoToDefinition implements OmniSharp.IFeature {
             this.marker.bufferMarker.range.compare(wordRange) === 0)
             return;
 
+        var addMark = () => {
+            this.removeMarker();
+            if (highlight.enabled && highlight.active)
+                this.marker = editor.markBufferRange(wordRange);
+            var decoration = editor.decorateMarker(this.marker, { type: 'highlight', class: 'gotodefinition-underline' });
+        };
+
+        // TODO: Remove this and uncomment below code
+        //       once we have metadata information.
         Omni.request(editor, client => client.gotodefinition({
             Line: bufferPt.row,
             Column: bufferPt.column,
             FileName: editor.getURI()
-        }))
-            .subscribe(data => {
-                if (data.FileName !== null) {
-                    this.removeMarker();
-                    this.marker = editor.markBufferRange(wordRange);
-                    var decoration = editor.decorateMarker(this.marker, { type: 'highlight', class: 'gotodefinition-underline' })
-                }
-            });
+        })).where(data => !!data.FileName)
+            .subscribe(data => addMark());
+
+        /*if (highlight.enabled && highlight.active) {
+            var scopes: string[] = (<any>editor.scopeDescriptorForBufferPosition(bufferPt)).scopes;
+            if (identifierRegex.test(_.last(scopes))) {
+                addMark();
+            }
+        } else {
+            // If enhanced highlighting is off, fallback to the old method.
+            Omni.request(editor, client => client.gotodefinition({
+                Line: bufferPt.row,
+                Column: bufferPt.column,
+                FileName: editor.getURI()
+            })).where(data => !!data.FileName)
+                .subscribe(data => addMark());
+        }*/
     }
 
-    private pixelPositionFromMouseEvent(editorView, event: MouseEvent) {
+    private pixelPositionFromMouseEvent(editor: Atom.TextEditor, editorView, event: MouseEvent) {
         var clientX = event.clientX, clientY = event.clientY;
         var linesClientRect = this.getFromShadowDom(editorView, '.lines')[0].getBoundingClientRect();
         var top = clientY - linesClientRect.top;
         var left = clientX - linesClientRect.left;
+        top += editor.displayBuffer.getScrollTop();
+        left += editor.displayBuffer.getScrollLeft();
         return { top: top, left: left };
     }
 
