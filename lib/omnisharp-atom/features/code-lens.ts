acb627895382cd137fb9f5d1ec8dec44e54405ed
David Driscoll <david.driscoll@gmail.com>
2015-08-11 23:16:47 -0400

    Coverted code-lens code

--- lib/omnisharp-atom/features/code-lens.ts
+++ lib/omnisharp-atom/features/code-lens.ts
@@ -1,89 +1,82 @@
 import _ = require('lodash');
 //import spacePenViews = require('atom-space-pen-views')
 //var $ = spacePenViews.jQuery;
-import {CompositeDisposable, Observable} from "rx";
+import {CompositeDisposable, Observable, Disposable} from "rx";
 import Omni = require('../../omni-sharp-server/omni');
 import {DriverState} from "omnisharp-client"
 
 class CodeLens implements OmniSharp.IFeature {
     private disposable: Rx.CompositeDisposable;
+    private activeMarkers: Atom.Marker[] = [];
 
-  public activate() {
-      this.disposable.add(Omni.activeEditor.where(z => !!z).subscribe(editor => {
-          var cd = new CompositeDisposable();
+    public activate() {
+        this.disposable.add(Omni.switchActiveEditor((editor, cd) => {
+            editor.getBuffer().onDidStopChanging(() => this.updateCodeLens(editor));
+            editor.getBuffer().onDidSave(() => this.updateCodeLens(editor));
+            editor.getBuffer().onDidReload(() => this.updateCodeLens(editor));
 
-          cd.add(Omni.activeEditor.where(active => active !== editor).subscribe(() => {
-              cd.dispose();
-              this.disposable.remove(cd);
-          }));
-      }));
+            editor.onDidChangeScrollTop((top) => {
+                var editorView = $(atom.views.getView(editor));
+                var line = $(".codelens", this.getFromShadowDom(editorView, ".scroll-view"));
 
-    this.atomSharper.onEditor((editor: Atom.TextEditor) => {
-      editor.getBuffer().onDidStopChanging(() => this.updateCodeLens(editor));
-      editor.getBuffer().onDidSave(() => this.updateCodeLens(editor));
-      editor.getBuffer().onDidDelete(() => this.updateCodeLens(editor));
-      editor.getBuffer().onDidReload(() => this.updateCodeLens(editor));
+                line.css("top", top + "px");
+            });
 
-      editor.onDidChangeScrollTop((top) => {
+            this.updateCodeLens(editor);
+
+            cd.add(Disposable.create(() => {
+                _.each(this.activeMarkers, marker => marker.destroy());
+                this.activeMarkers = [];
+            }))
+        }));
+    }
+
+    public dispose() {
+        this.disposable.dispose();
+    }
+
+    public updateCodeLens(editor: Atom.TextEditor) {
+        var lineHeight = "+=" + editor.getLineHeightInPixels() + "px";
         var editorView = $(atom.views.getView(editor));
-        var line = $(".codelens", this.getFromShadowDom(editorView, ".scroll-view"));
 
-        line.css("top", top + "px");
+        var initialBufferPos = editor.getCursorBufferPosition();
 
-      });
-    });
+        Omni.request(editor, solution =>
+            solution.currentfilemembersasflat(solution.makeRequest(editor)))
+            .flatMap(fileMembers => Observable.from(fileMembers))
+            .concatMap(fileMember =>
+                Omni.request(editor, solution => solution.findusages({
+                    Column: fileMember.Column + 1,
+                    FileName: editor.getPath(),
+                    Line: fileMember.Line,
+                })
+                    .map(response => ({ response, fileMember }))))
+            .toArray()
+            .subscribe(responses => {
 
-    Omni.registerConfiguration(client => {
-      client.state.subscribe(state => {
-        if (state === DriverState.Connected)
-          this.updateCodeLens(null);
-      });
-    });
+                _.each(this.activeMarkers, marker => marker.destroy());
+                this.activeMarkers = [];
+                
+                _.each(responses, (ctx) => {
+                    var {response, fileMember} = ctx;
 
-  }
+                    var range = editor.getBuffer().rangeForRow(fileMember.Line - 1);
+                    var marker = editor.markBufferRange(range);
+                    this.activeMarkers.push(marker);
 
-  public updateCodeLens(editor: Atom.TextEditor) {
-    var self = this; // yay for scoping, I always forget in typhadowescript, what this does, big arrow should bind this to the class instance, right?
-    if (editor == undefined || Omni.client === undefined || Omni.client.currentState !== DriverState.Connected) return;
+                    var decorationParams = {
+                        type: "line"
+                    };
+                    var decoration = editor.decorateMarker(marker, decorationParams);
+                });
+            });
+    }
 
-    var lineHeight = "+=" + editor.getLineHeightInPixels() + "px";
-    var editorView = $(atom.views.getView(editor));
-
-    var initialBufferPos = editor.getCursorBufferPosition();
-
-    _.debounce(() => {
-      var request = <OmniSharp.Models.FormatRangeRequest>Omni.makeRequest(editor);
-      Omni.client.currentfilemembersasflatPromise(request)
-        .then((fileMembers) => {
-          _.forEach(fileMembers, function(fileMember: OmniSharp.Models.QuickFix) {
-            console.log(fileMember);
-
-            var req = <OmniSharp.Models.FindUsagesRequest>{
-              Column: fileMember.Column + 1,
-              FileName: request.FileName,
-              Line: fileMember.Line,
-            };
-            //go get the count of the usages
-            Omni.client.findusagesPromise(req)
-              .then((findUsagesResponse) => {
-                var range = editor.getBuffer().rangeForRow(req.Line - 1);
-                var marker = editor.markBufferRange(range);
-                var decorationParams = {
-                    type : "line",
-                    //class : `codelens-${findUsagesResponse.QuickFixes.length}`
-                };
-                var decoration = editor.decorateMarker(marker, decorationParams);
-              });
-          });
-        });
-    }, 500)();
-  }
-
-  private getFromShadowDom(element: JQuery, selector: string): JQuery {
-    var el = element[0];
-    var found = (<any> el).rootElement.querySelectorAll(selector);
-    return $(found[0]);
-  }
+    private getFromShadowDom(element: JQuery, selector: string): JQuery {
+        var el = element[0];
+        var found = (<any> el).rootElement.querySelectorAll(selector);
+        return $(found[0]);
+    }
 }
 
 export = CodeLens;
