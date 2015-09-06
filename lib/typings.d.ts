482a9fa26283293d9435688c6d8e62c51f10deb9
David Driscoll <david.driscoll@gmail.com>
2015-09-05 23:03:16 -0400

    Refactored status bar view to use Web Components, instead of React

--- lib/typings.d.ts
+++ lib/typings.d.ts
@@ -47,3 +47,17 @@ declare module OmniSharp {
     }
 
 }
+interface WebComponent {
+    createdCallback?: () => void;
+    attachedCallback?: () => void;
+    detachedCallback?: () => void;
+    attributeChangedCallback?: (attrName: string, oldVal: any, newVal: any) => void;
+}
+
+declare module "fastdom" {
+    export function read(cb: Function): any;
+    export function write(cb: Function): any;
+    export function defer(frames: number, cb: Function): any;
+    export function defer(cb: Function): any;
+    export function clear(caller: any): void;
+}
