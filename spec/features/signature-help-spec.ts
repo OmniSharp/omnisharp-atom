bbe548bbb4c015c32ac404678739dbd66a1ac217
David Driscoll <david.driscoll@gmail.com>
2015-09-27 14:43:09 -0400

    Added basic support for signatures, next to style

---
+++ spec/features/signature-help-spec.ts
@@ -0,0 +1,21 @@
+import Client = require("../../lib/omni-sharp-server/client");
+import Omni = require('../../lib/omni-sharp-server/omni');
+import {Observable, CompositeDisposable} from "rx";
+import {setupFeature, restoreBuffers, openEditor} from "../test-helpers";
+
+describe('Signature Help', () => {
+    setupFeature(['features/signature-help']);
+
+    it('adds commands', () => {
+        var disposable = new CompositeDisposable();
+
+        runs(() => {
+            var commands: any = atom.commands;
+
+            expect(commands.registeredCommands['omnisharp-atom:signature-help']).toBeTruthy();
+            disposable.dispose();
+        });
+    });
+
+
+});
